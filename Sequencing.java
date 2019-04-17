a81fd0f9932e90b8ceabf3fb340295e9bd0fd05f
42ea <erian@axis.com>
2019-04-17 15:27:07 +0200

    added test variable

--- Sequencing.java
+++ Sequencing.java
@@ -224,6 +224,7 @@ public class Sequencing extends Thread{
 					System.out.println(e);
 				}
 				refgen.setRef(5);
+				double test = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
 
 			}
 			case BIG: {
